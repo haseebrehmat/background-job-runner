e2acb50dd869882a7bf9f66a23e907623fb117f6
haseebrehmat <engineer.haseeb.rehmat@gmail.com>
2024-11-14 15:02:02 -0800

    refactor(Helpers): Add neccessary comments to each helper for readablity

--- app/Helpers/BackgroundJobHelper.php
+++ app/Helpers/BackgroundJobHelper.php
@@ -4,6 +4,17 @@ use App\Services\BackgroundJobRunner;
 
 if (!function_exists('runBackgroundJob'))
 {
+    /**
+     * Run a background job on the specified class and method with parameters.
+     *
+     * This function uses the BackgroundJobRunner service to execute a job in the
+     * background. It will automatically determine if the system is running on
+     * Windows or a Unix-based system and execute the job accordingly.
+     *
+     * @param string $class The class name of the job to run.
+     * @param string $method The method name of the job to run.
+     * @param array $params The parameters to pass to the job method.
+     */
     function runBackgroundJob($class, $method, $params = [])
     {
         $runner = new BackgroundJobRunner();
@@ -21,16 +32,35 @@ if (!function_exists('runBackgroundJob'))
         }
     }
 
+    /**
+     * Determine if the OS is Windows
+     *
+     * @return bool
+     */
     function isWindows()
     {
         return strtoupper(substr(PHP_OS, 0, 3)) === 'WIN';
     }
 
+    /**
+     * Execute job in Windows environment
+     *
+     * @param string $class
+     * @param string $method
+     * @param string $encodedParams
+     */
     function executeWindowsJob($class, $method, $encodedParams)
     {
         pclose(popen("start /B php artisan job:run \"$class\" \"$method\" $encodedParams", "r"));
     }
 
+    /**
+     * Execute job in Unix-based environment
+     *
+     * @param string $class
+     * @param string $method
+     * @param string $encodedParams
+     */
     function executeUnixJob($class, $method, $encodedParams)
     {
         shell_exec("php artisan job:run \"$class\" \"$method\" $encodedParams > /dev/null 2>/dev/null &");
