9091114da1cd93fd9e6a4ec0d7be6325102b8474
haseebrehmat <engineer.haseeb.rehmat@gmail.com>
2024-11-14 14:29:52 -0800

    refactor(config): Update `allowed_classes`

--- config/background_jobs.php
+++ config/background_jobs.php
@@ -2,6 +2,9 @@
 
 return [
     'allowed_classes' => [
+        \App\Jobs\SendEmailJob::class,
+        \App\Jobs\GenerateReportJob::class,
+        \App\Jobs\CleanupOldRecordsJob::class,
     ],
     'retries'         => env('BACKGROUND_JOB_MAX_RETRIES', 3),
 ];
